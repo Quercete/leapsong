3d07c4afe8c15c820a330686f9b5def27ae6a1c2
loxygenK <loxygen.k@gmail.com>
2021-02-09 14:17:39 +0900

    🏷️ Added key_id to Key model

--- src/model/key.py
+++ src/model/key.py
@@ -6,12 +6,14 @@ from .key_data.usage import Usage
 @dataclasses.dataclass
 class Key:
     key_armor: str
+    key_id: str
     key_user_id: KeyUserId
     usage: Usage
 
     def to_dict(self) -> dict:
         return {
             "key_armor": self.key_armor,
+            "key_id": self.key_id,
             "key_user_id": {
                 "name": self.key_user_id.name,
                 "email": self.key_user_id.email,
@@ -28,6 +30,7 @@ class Key:
     def from_dict(self, data_dict: dict):
         return Key(
             key_armor=data_dict["key_armor"],
+            key_id=data_dict["key_id"],
             key_user_id=KeyUserId(
                 name=data_dict["key_user_id"]["name"],
                 email=data_dict["key_user_id"]["email"],
