9e1d9c864ed187f96e355d8f105a3c4be3df26e7
loxygenK <loxygen.k@gmail.com>
2021-02-09 17:54:16 +0900

    ✨ Implemented reading from key armor

--- src/model/key.py
+++ src/model/key.py
@@ -1,4 +1,7 @@
 import dataclasses
+import pgpy
+from pgpy.constants import KeyFlags
+
 from .key_data.user_id import KeyUserId
 from .key_data.usage import Usage
 
@@ -7,13 +10,42 @@ from .key_data.usage import Usage
 class Key:
     key_armor: str
     key_id: str
+    fingerprint: str
     key_user_id: KeyUserId
     usage: Usage
+
+    @staticmethod
+    def from_key_armor(key_armor: str):
+        try:
+            parsed_key = pgpy.PGPKey.from_blob(key_armor.replace("\\n", "\n"))
+            key: pgpy.PGPKey = parsed_key[0]
+            long_key_id = list(parsed_key[1].keys())[0][0]
+        except ValueError:
+            return None
+
+        return Key(
+            key_armor=key_armor,
+            key_id=long_key_id,
+            fingerprint=key.fingerprint.replace(" ", ""),
+            key_user_id=KeyUserId(
+                name=key.userids[0].name,
+                email=key.userids[0].email,
+                comment=key.userids[0].comment,
+            ),
+            usage=Usage(
+                for_authenticate=KeyFlags.Authentication in key._get_key_flags(),
+                for_certify=KeyFlags.Certify in key._get_key_flags(),
+                for_encrypt=KeyFlags.EncryptStorage in key._get_key_flags()
+                            and KeyFlags.EncryptCommunications in key._get_key_flags(),
+                for_sign=KeyFlags.Sign in key._get_key_flags()
+            )
+        )
 
     def to_dict(self) -> dict:
         return {
             "key_armor": self.key_armor,
             "key_id": self.key_id,
+            "fingerprint": self.fingerprint,
             "key_user_id": {
                 "name": self.key_user_id.name,
                 "email": self.key_user_id.email,
@@ -32,6 +64,7 @@ class Key:
         return Key(
             key_armor=data_dict["key_armor"],
             key_id=data_dict["key_id"],
+            fingerprint=data_dict["fingerprint"],
             key_user_id=KeyUserId(
                 name=data_dict["key_user_id"]["name"],
                 email=data_dict["key_user_id"]["email"],
