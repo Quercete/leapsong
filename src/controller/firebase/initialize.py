dfb6d93c4bd808cf645932c30bcd9c9fb1bd0629
Colk-tech <colk.tech@gmail.com>
2021-02-09 13:32:07 +0900

    [追加] Firebaseの初期化を追加

---
+++ src/controller/firebase/initialize.py
@@ -0,0 +1,24 @@
+import base64
+
+import firebase_admin
+
+from config import CREDENTIAL_FILE_NAME, CREDENTIAL_B64, FIREBASE_SETTINGS
+
+
+def __create_credential_file() -> None:
+    with open(CREDENTIAL_FILE_NAME, "wb") as f:
+        f.write(base64.b64decode(CREDENTIAL_B64))
+
+
+def __get_credential() -> firebase_admin.credentials.Certificate:
+    credential = firebase_admin.credentials.Certificate(FIREBASE_SETTINGS)
+
+    return credential
+
+
+def initialize() -> None:
+    __create_credential_file()
+
+    CREDENTIAL = __get_credential()
+
+    firebase_admin.initialize_app(CREDENTIAL, FIREBASE_SETTINGS)
