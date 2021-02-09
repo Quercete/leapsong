6f0031672967201672fee8c589355eab04c0de24
Colk-tech <colk.tech@gmail.com>
2021-02-09 17:46:55 +0900

    [削除] CloudStorageいらんし

--- src/controller/firebase/cloud_storage.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from google.cloud import storage
-
-from config import CREDENTIAL_FILE_NAME
-
-
-storage_client: storage.Client = \
-    storage.Client.from_service_account_json(CREDENTIAL_FILE_NAME)
